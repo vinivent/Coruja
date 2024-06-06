6a837f7c3eb43da2a8bc9ccd36d82ae9faa81510
Vinicius <vini.ventura999@gmail.com>
2024-06-06 15:15:39 -0300

    implementando setup e loop

--- Coruja.ino
+++ Coruja.ino
@@ -9,9 +9,22 @@ int index = 0;
 char messages[] = {'0', '1', '2', '3'};
 
 void setup() {
-  
+  mySerial.begin(9600);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
 }
 
 void loop() {
-  
+  buttonState2 = digitalRead(buttonPin2);
+  if (buttonState2 == HIGH) {
+    mySerial.write(messages[index]);
+    index = (index + 1) % 4; // Atualiza o índice, volta para 0 se atingir 4
+    delay(500);
+  }
+
+  buttonState3 = digitalRead(buttonPin3);
+  if (buttonState3 == HIGH) {
+    mySerial.write('E');
+    delay(500);
+  }
 }
