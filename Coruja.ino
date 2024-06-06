323fa3b1529844f69bc23833cf10cece2f7c60c1
Vinicius <vini.ventura999@gmail.com>
2024-06-06 15:15:09 -0300

    importando biblioteca e inicializando variaveis

--- Coruja.ino
+++ Coruja.ino
@@ -1,3 +1,13 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+const int buttonPin2 = 4;
+const int buttonPin3 = 5;
+int buttonState2 = 0;
+int buttonState3 = 0;
+int index = 0;
+char messages[] = {'0', '1', '2', '3'};
+
 void setup() {
   
 }
